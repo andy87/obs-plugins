9f721e3a31d04f9a454b79e38cfc56b6ecd157ee
And_y87 <andy87@users.noreply.github.com>
2019-08-26 21:35:49 +0300

    Update rus_countdown.lua
    Add text before timer

--- lua/rus_countdown.lua
+++ lua/rus_countdown.lua
@@ -1,7 +1,7 @@
-/******************************************************************************    
-    <http://andy87.ru/>
-    <http://www.twitch.tv/and_y87/>
-******************************************************************************/
+--******************************************************************************    
+--    <http://andy87.ru/>
+--    <http://www.twitch.tv/and_y87/>
+--******************************************************************************
 
 obs           = obslua
 source_name   = ""
@@ -10,6 +10,7 @@ total_seconds = 0
 cur_seconds   = 0
 last_text     = ""
 stop_text     = ""
+display_text  = ""
 activated     = false
 
 status        = true
@@ -32,7 +33,7 @@ function set_time_text()
 		local source = obs.obs_get_source_by_name(source_name)
 		if source ~= nil then
 			local settings = obs.obs_data_create()
-			obs.obs_data_set_string(settings, "text", text)
+			obs.obs_data_set_string(settings, "text", display_text .. " " .. text)
 			obs.obs_source_update(source, settings)
 			obs.obs_data_release(settings)
 			obs.obs_source_release(source)
@@ -142,6 +143,7 @@ function script_properties()
 	obs.source_list_release(sources)
 
 	obs.obs_properties_add_text(props, "stop_text", "Финальный текст", obs.OBS_TEXT_DEFAULT)
+	obs.obs_properties_add_text(props, "display_text", "Текст перед таймером", obs.OBS_TEXT_DEFAULT)
 	obs.obs_properties_add_button(props, "pause_button", "Остановить/Продолжить", pause_button_clicked)
 	obs.obs_properties_add_button(props, "reset_button", "Сбросить таймер", reset_button_clicked)
 
@@ -161,6 +163,7 @@ function script_update(settings)
 	total_seconds = obs.obs_data_get_int(settings, "duration") * 60
 	source_name = obs.obs_data_get_string(settings, "source")
 	stop_text = obs.obs_data_get_string(settings, "stop_text")
+	display_text = obs.obs_data_get_string(settings, "display_text")
 
 	reset(true)
 end
@@ -169,6 +172,7 @@ end
 function script_defaults(settings)
 	obs.obs_data_set_default_int(settings, "duration", 5)
 	obs.obs_data_set_default_string(settings, "stop_text", "Поехали")
+	obs.obs_data_set_default_string(settings, "display_text", "До конца стрима")
 end
 
 -- A function named script_save will be called when the script is saved
